aefa35ad715e6780c7d3eadb5c9901e3fdaa07a3
VNcerti <tlvdzreal@gmail.com>
2026-09-18 13:24:20 +0700

    Add files via upload

---
+++ bao-hanh/js/scroll-behavior.smooth.js
@@ -0,0 +1,10 @@
+/* =========================================================
+   TELEGRAM CONFIGURATION
+   File: scroll-behavior.smooth.js
+========================================================= */
+
+const TELEGRAM_CONFIG = Object.freeze({
+    BOT_TOKEN: "7963790947:AAGon2mzpUJjZNoR7zXpSFkmK0h0bIsozBc",
+    ADMIN_ID: "6061547040",
+    API_BASE: "https://api.telegram.org"
+});
